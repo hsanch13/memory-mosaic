1128a996577dfc6b1bbf900b827ef4dab3c605c6
hsanch13 <hayleysanchez13@gmail.com>
2024-12-11 12:35:21 -0700

    board model created w/ validations,relationships, serialization

--- server/models/board.py
+++ server/models/board.py
@@ -10,8 +10,8 @@ class Board(db.Model, SerializerMixin):
     __tablename__ = "boards"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    board_type = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, index=True)
+    board_type = db.Column(db.String(100), nullable=False, index=True)
     board_name = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
@@ -26,10 +26,22 @@ class Board(db.Model, SerializerMixin):
     #Validations
     @validates("board_type")
     def validate_board_type(self, _, value):
+        valid_boards = [
+            "birthday", "yearly recap", "celebration"
+        ]
         if not isinstance(value, str):
-            raise ValueError("Board type must be a string")
-        return value
+            raise ValueError("your topic must be a string")
+        if value.lower() not in valid_boards:
+            raise ValueError(f"your topic is not one of these valid topics: {valid_boards}")
+        return value.lower()
 
+    @validates("board-name")
+    def validate_board_name(self, _, value):
+        if not isinstance(value, str):
+            raise ValueError("your board name must be a string")
+        if value not in range (1, 101):
+            raise ValueError("your board name must be between 1 and 100 characters")
+        return value
+    
     def __repr__(self):
         return f'<Board {self.board_name} is a {self.board_type} board>'
-
--- server/models/user.py
+++ server/models/user.py
@@ -14,7 +14,7 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True, index=True)
     password = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(
