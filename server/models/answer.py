fae2f080bcd083464c05427dd62ff74462adc0f9
hsanch13 <hayleysanchez13@gmail.com>
2024-12-11 13:27:12 -0700

    updates to answer model with validations, relationships, serialization

--- server/models/answer.py
+++ server/models/answer.py
@@ -8,23 +8,31 @@ import re
 
 
 # Model set up
-class Answer(db.Model):
+class Answer(db.Model, SerializerMixin):
     __tablename__ = "answers"
 
     id = db.Column(db.Integer, primary_key=True)
-    board_id = db.Column(db.Integer, db.ForeignKey("board.id"), nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey("question.id"), nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.id"), nullable=False, index=True)
+    question_id = db.Column(db.Integer, db.ForeignKey("question.id"), nullable=False, index=True)
     answer_text = db.Column(db.String(500))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
     # Relationship
     board = db.relationship("Board", back_populates="answers")
     question = db.relationship("Question", back_populates="answers")
-    media = db.relationship("Media", back_populates="answer")
+    media = db.relationship("Media", back_populates="answer", cascade="all, delete-orphan")
 
     # Serialization rules
+    serialize_rules = ["-board", "-question", "-media"]
 
     # Validations
+    @validates("answer_text")
+    def validate_answer_text(self, _, value):
+        if not isinstance(value, str):
+            raise ValueError("answer text must be a string.")
+        if not (1 <= len(value) <= 500):
+            raise ValueError("answer text must be between 1 and 500 characters")
+        return value
 
     def __repr__(self):
         return f"<Answer {self.answer_text}>"
