103ca392cb996967124b612c85217079aec25548
hsanch13 <hayleysanchez13@gmail.com>
2024-12-13 10:59:53 -0700

    routes tested and working for auth, questions, board

--- server/routes/board/boards.py
+++ server/routes/board/boards.py
@@ -25,27 +25,3 @@ class Boards(Resource):
             return make_response(new_board.to_dict(), 201)
         except Exception as e:
             return make_response({"error": str(e)}, 500)
-
-    def patch(self, id):
-        try:
-            board = Board.query.get(id)
-            if not board:
-                return make_response({"error": f"Board with id {id} not found"}, 404)
-            data = request.get_json()
-            board.board_type = data.get("board_type", board.board_type)
-            board.board_name = data.get("board_name", board.board_name)
-            db.session.commit()
-            return make_response(board.to_dict(), 200)
-        except Exception as e:
-            return make_response({"error": str(e)}, 500)
-
-    def delete(self, id):
-        try:
-            board = Board.query.get(id)
-            if not board:
-                return make_response({"error": f"Board with id {id} not found"}, 404)
-            db.session.delete(board)
-            db.session.commit()
-            return make_response({"message": f"Board with id {id} deleted"}, 200)
-        except Exception as e:
-            return make_response({"error": str(e)}, 500)
--- server/routes/board/boards_by_id.py
+++ server/routes/board/boards_by_id.py
@@ -1,6 +1,7 @@
 from models import Board
 from flask import make_response
-from flask_restful import Resource
+from flask_restful import Resource, request
+from config import db
 
 class BoardsById(Resource):
     def get(self, id):
@@ -9,5 +10,29 @@ class BoardsById(Resource):
             if not board:
                 return make_response({"error": f"Board with id {id} not found"}, 404)
             return make_response(board.to_dict(), 200)
+        except Exception as e:
+            return make_response({"error": str(e)}, 500)
+        
+    def patch(self, id):
+        try:
+            board = Board.query.get(id)
+            if not board:
+                return make_response({"error": f"Board with id {id} not found"}, 404)
+            data = request.get_json()
+            board.board_type = data.get("board_type", board.board_type)
+            board.board_name = data.get("board_name", board.board_name)
+            db.session.commit()
+            return make_response(board.to_dict(), 200)
+        except Exception as e:
+            return make_response({"error": str(e)}, 500)
+
+    def delete(self, id):
+        try:
+            board = Board.query.get(id)
+            if not board:
+                return make_response({"error": f"Board with id {id} not found"}, 404)
+            db.session.delete(board)
+            db.session.commit()
+            return make_response({"message": f"Board with id {id} deleted"}, 200)
         except Exception as e:
             return make_response({"error": str(e)}, 500)
